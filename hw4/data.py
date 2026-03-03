c55860aaba236e0bf9b49f7849dbd775aae4a19e
qinglang-wang <qinglang.wang.cmu@gmail.com>
2026-03-03 03:10:36 -0800

    [Refactor] COCO Features

--- hw4/data.py
+++ hw4/data.py
@@ -118,6 +118,22 @@ YOLO_TO_COCO = [
     85, 86, 87, 88, 89, 90
 ]
 
+# COCO 80 class names (in order matching YOLO indices 0-79)
+COCO_CLASS_NAMES = [
+    'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck',
+    'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench',
+    'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra',
+    'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+    'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+    'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+    'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange',
+    'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+    'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse',
+    'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink',
+    'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
+    'hair drier', 'toothbrush'
+]
+
 def load_coco_annotations(coco_dir='data'):
     """
     Load COCO 2017 val annotations.
